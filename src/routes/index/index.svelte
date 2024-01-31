78245c204d92cffd71ff88347b069b9b53a7ca32
KevinH2028 <zacdog20@gmail.com>
2024-01-31 14:20:24 -0500

    Add content to the index page.
    Added a short introduction to the game, with some basic stylization.

--- src/routes/index/index.svelte
+++ src/routes/index/index.svelte
@@ -1,2 +1,37 @@
-
-  <h1>Hello, SvelteKit!</h1>
+<html lang="en">
+  <h1 id="head">Welcome to (Turn-based-game)!</h1>
+  <div class="info">
+    <p id="intro">Our game was designed to mix the daily aspect of games like wordle, and the dynamic, exciting aspects of a Roguelike. Every 
+      day you, the player, are provided with one life to travel the world and find resources, battle monsters, and level up your many skills.</p>
+    <p>If you would like to try our game out, you can play our game <a href="/sign-up">here!</a> If you are enjoying our game, don't forget to make an account 
+      to save all of your progress, or if you are a returning player, remember to <a href="/login">log in</a> to save your progress.</p>
+  </div>
+</html>
+<style>
+  html {
+    background-color: rgb(252, 164, 164);
+  }
+  * {
+    padding-block: 5px;
+    margin-left: 20px;
+  }
+  h1 {
+    font-size: 20px;
+  }
+  p {
+    padding-inline: 10px;
+    margin-inline: 20px;
+  }
+  .info {
+    background-color: rgb(231, 128, 128);
+    border: rgb(102, 5, 5) 3px solid;
+    border-radius: 10px;
+    box-shadow: 3px 3px 9px rgba(102, 5, 5, 0.3);
+  }
+  a {
+    padding: 0px;
+    margin: 0px;
+    text-decoration: underline;
+    color: green;
+  }
+</style>
